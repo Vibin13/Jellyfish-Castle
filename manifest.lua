5e311765f71e3e0979144bcc5e609a2fbb0b1eb4
Vibin13 <164310688+Vibin13@users.noreply.github.com>
2026-06-05 14:48:38 -0400

    Add files via upload

---
+++ manifest.lua
@@ -0,0 +1,37 @@
+return {
+    house = {
+        house_pos = Vector3.new(-12000,4000,3000),
+        unique = "house_{e93b33d4-181b-45c0-8f7d-c5f0d96db570}",
+        active_addons = {
+        },
+        building_type = "micro_2023",
+        house_id = 17,
+    },
+    textures = {
+        Room2 = {
+            floors = "checkers",
+            walls = "white",
+        },
+        Room1 = {
+            floors = "woodenplanks",
+            walls = "bluestripes",
+        },
+        Room3 = {
+            floors = "expensivepurplecarpet",
+            walls = "pink",
+        },
+    },
+    metadata = {
+        totalFurniture = 3875,
+        originalOwner = "Kittkatt11223",
+        date = "2026-01-17 09:58:58",
+        savedBy = "KingPhoGood",
+        totalChunks = 2,
+        version = "2.0",
+        chunkSize = 2000,
+        chunked = true,
+        timestamp = 1768679938,
+        savedByUserId = 8478321097,
+        totalValue = 110331,
+    },
+}
